b508cb60e1c923dc7f603fce48465c3c4d355826
Emily Purser <pur20008@byui.edu>
2022-04-20 10:57:14 -0600

    fixed the problem of my list items not showing in table of contents

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 function getTableOfContents() {
-    var List = document.getElementsById("main-list");
+    let list = document.getElementById("main-list");
     const links = [
         {
             label: "Week 1 Notes", 
             url: "week1/index.html"
         }
-    ]
-    let dataTable;
+    ]  
     links.forEach(element => {
         console.log('I am in the for each statement');
-        dataTable = `<li class="table-links">`;
-        dataTable += `<a href='/WDD330-Portfolio/${element.url}>${element.label}</a>`;
-        dataTable += `</li>`;
+        let li = document.createElement("li");
+        let label = element.label;
+        let url = element.url;
+        li.innerHTML = "<a href='" + url + "'>" + label + "</a>";
+        list.appendChild(li);
     });
-    List.innerHTML = dataTable;
 }
